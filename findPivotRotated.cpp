0c038eee699fe3969e9cd64fa31b29ff1f59d015
Utsav-patell <22bt04105@gsfcuniversity.ac.in>
2024-08-05 00:48:30 +0530

    "Pivot in Rotated Array"

---
+++ findPivotRotated.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        int n = nums.size()-1;
+        int start = 0;
+        int end = n;
+        int mid = start + (end-start)/2;
+        if(nums[n]>nums[start]){
+            return nums[start];
+        }
+        while(start<end){
+            if(nums[mid]>=nums[0]){
+                start = mid+1;
+            }
+            else{
+                end = mid;
+            }
+            mid = start + (end-start)/2;
+        }
+        return nums[start];
+        
+    }
+};
